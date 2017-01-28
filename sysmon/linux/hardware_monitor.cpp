7019b9299ea18ed556331a0e652cc5f28b05a8ee
Debdipta Ghosh <debdipta1078@gmail.com>
2017-01-28 18:29:16 +0000

    Structure1

--- sysmon/linux/hardware_monitor.cpp
+++ sysmon/linux/hardware_monitor.cpp
@@ -1,7 +1,18 @@
-
+#include <iostream>
+#include <list>
 #include "hardware_monitor.h"
+#include "cpu_monitor.h"
+#include "io_monitor.h"
 
-hardware_monitor::hardware_monitor( char* _name)
+hardware_monitor* hardware_monitor::_hardware_monitor = NULL;
+hardware_monitor* hardware_monitor::get_instance()
+{
+    if (NULL == _hardware_monitor)
+        _hardware_monitor =  new hardware_monitor("hardware Monitor");
+    return _hardware_monitor;        
+}
+
+hardware_monitor::hardware_monitor( const char* _name)
 {
     name = _name;
 }
@@ -9,4 +20,11 @@ hardware_monitor::hardware_monitor( char* _name)
 hardware_monitor::~hardware_monitor()
 {
 
+}
+
+bool hardware_monitor::register_monitors() 
+{
+    monitor_list.push_back(new cpu_monitor("CPU"));
+    monitor_list.push_back(new io_monitor("IO"));
+    return true;
 }
--- sysmon/linux/hardware_monitor.h
+++ sysmon/linux/hardware_monitor.h
@@ -1,12 +1,28 @@
 #ifndef __HARDWARE_MONITOR__
 #define __HARDWARE_MONITOR__
 
+#include <iostream>
+#include <list>
+#include "monitor_interface.h"
+
+/*
+* class hardware_monitor maintains a list of monitor objects alike cpu monitor, io monitor, network monitor. When asked,
+it calls all objects for updated information, and send back to caller
+*/
+
 class hardware_monitor
 {
     const char* name;
+    std::list<monitor_interface*> monitor_list;
+    static hardware_monitor* _hardware_monitor; 
+    hardware_monitor( const char* );
+protected:
+    bool get_update();
 public:
-    hardware_monitor(char* );
     ~hardware_monitor();
+    static hardware_monitor* get_instance();
+    bool register_monitors();
+
 };
 
 #endif
