0315b9da3bbf40e93e3f204875f7e9226fb69de4
Debdipta Ghosh <debdipta1078@gmail.com>
2017-11-29 12:08:28 +0000

    Makefile cpu_monitor.cpp cpu_monitor.h linux/linux_cpu_monitor.cpp linux/linux_cpu_monitor.h monitor_interface.h

--- sysmon/cpu_monitor.h
+++ sysmon/cpu_monitor.h
@@ -4,16 +4,20 @@
 #include "monitor_interface.h"
 #include "hardware_monitor.h"
 #include "linux/linux_cpu_monitor.h"
+#include <string>
+#include <iostream>
 
 /* This class is registered to hardware monitor */
 class cpu_monitor: public monitor_interface
 {
     const char* name;
-    char output[2048];
+    std::string format;
+    std::string header;
+    std::string footer;
     monitor_interface* monitor;
 public:
     cpu_monitor(const char*);
     ~cpu_monitor();
-    char* collect_data();
+    const char* collect_data();
 };
 #endif
